e01b27cddb86cdfc21dd8ae818abad3e94949a4e
Thang Nguyen <thang.1.nguyen@kcl.ac.uk>
2023-01-22 03:37:14 +0000

    Add UserManager model
    UserManager model is needed to replace username with email for authentication

--- PST/models.py
+++ PST/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+"""Models in the PST app"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import BaseUserManager
+
+class UserManager(BaseUserManager):
+    '''User manager that uses email for authentication instead of usernames'''
+
+    def create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email = email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
