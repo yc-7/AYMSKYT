a6c5172c278caeeb1e5ad816b9713d30178123d8
Kyron Caesar <dragonitezball@gmail.com>
2023-01-24 14:25:51 +0000

    Add category model

--- PST/models.py
+++ PST/models.py
@@ -23,3 +23,12 @@ class User(AbstractUser):
 
     def __str__(self):
         return  self.first_name+" "+self.last_name
+
+    
+
+class Category(models.Model):
+    """Model for expenditure categories"""
+
+    user = models.ForeignKey(User, blank = False, on_delete= models.CASCADE)
+    name = models.CharField(max_length = 50, blank = False)
+    budget = models.DecimalField(default = 0, max_digits = 6, decimal_places = 2)
