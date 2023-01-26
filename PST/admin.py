ad21f335aa57a1cd5daaaf8ff8a72f9669b5a1cc
Yu Han Chen <yu.h.chen@kcl.ac.uk>
2023-01-26 13:40:54 +0000

    Register User and Category models for admin interface

--- PST/admin.py
+++ PST/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import User, Category
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'email', 'first_name', 'last_name', 'is_active'
+    ]
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'user', 'name', 'budget'
+    ]
