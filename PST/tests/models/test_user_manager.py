875a3ef512cbe96d8903fb74af0664506cac5416
Yu Han Chen <yu.h.chen@kcl.ac.uk>
2023-01-26 14:03:55 +0000

    Add tests for user manager

---
+++ PST/tests/models/test_user_manager.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from PST.models import User
+
+class UserManagerModelTestCase(TestCase):
+    """Unit tests for the UserManager model"""
+
+    def test_create_user(self):
+        user = User.objects.create_user('johndoe@example.org', 'Password123')
+        self.assertTrue(isinstance(user, User))
+
+    def test_create_super_user(self):
+        user = User.objects.create_superuser('janedoe@example.org', 'Password123')
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(isinstance(user, User))
