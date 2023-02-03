c7328d0e7487572f7a74ca344b5c17785bf19366
Maja Szczepanczyk <majaszczepanczyk02@gmail.com>
2023-02-03 13:17:44 +0000

    Test edit profile view

---
+++ minted/tests/views/test_edit_profile_view.py
@@ -0,0 +1,48 @@
+from django.test import TestCase
+from django.urls import reverse
+from minted.models import User
+
+class EditProfileViewTestCase(TestCase):
+    
+    fixtures = ['minted/tests/fixtures/default_user.json']
+    
+    def setUp(self):
+        self.url = reverse('edit_profile')
+        self.form_input = {
+            'first_name': 'TestName',
+            'last_name': 'TestLastname',
+            'email': 'test@example.org',
+        }
+        self.user = User.objects.get(pk = 1)
+    
+    def test_view_uses_correct_template(self):
+        self.client.force_login(self.user)
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'edit_profile.html')
+        
+    def test_view_redirects_to_login_if_not_logged_in(self):
+        response = self.client.get(self.url)
+        self.assertRedirects(response, '/log_in/?next=' + self.url)
+        
+    def test_original_data(self):
+        self.assertEqual(self.user.first_name, 'John')
+        self.assertEqual(self.user.last_name, 'Doe')
+        self.assertEqual(self.user.email, 'johndoe@example.org')
+        
+        
+    def test_form_valid_data(self):
+        self.client.force_login(self.user)
+        response = self.client.post(self.url, data=self.form_input)
+        self.assertRedirects(response, '/profile/')
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.first_name, 'TestName')
+        self.assertEqual(self.user.last_name, 'TestLastname',)
+        self.assertEqual(self.user.email, 'test@example.org')
+        
+    def test_form_invalid_data(self):
+        self.client.force_login(self.user)
+        response = self.client.post(self.url, data={'email': 'invalid'})
+        self.assertTemplateUsed(response, 'edit_profile.html')
+        self.assertContains(response, 'Please correct the error below.')
+        
+    
