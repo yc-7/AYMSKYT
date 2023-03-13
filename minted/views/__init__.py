661d6c611147e603c599bd0d98231f32bb2392f6
Kyron Caesar <dragonitezball@gmail.com>
2023-03-13 14:52:57 +0000

    Update migrations

--- minted/views/__init__.py
+++ minted/views/__init__.py
@@ -3,8 +3,7 @@ from minted.views.analytics_views.analytics_views import *
 from .category_views import * 
 from .expenditure_views import *
 from .budget_views import *
-<<<<<<< HEAD
-from .friend_views import *
-=======
 from minted.views.notification_subscription_views.notification_subscription_views import *
->>>>>>> a3442697934d430998adcbb33368adf5645ffb91
+from .friend_views import *
+
+
