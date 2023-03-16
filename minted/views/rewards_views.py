a61183ea6734548de4b588f77cc0149f0132f672
Stephanie Lawal <stephanieelawal@gmail.com>
2023-03-16 12:38:19 +0000

    Create page for user to see their current claimed rewards

--- minted/views/rewards_views.py
+++ minted/views/rewards_views.py
@@ -4,14 +4,16 @@ from minted.forms import *
 from minted.models import *
 from .general_user_views.login_view_functions import *
 from django.contrib import messages
-from django.db.models import ExpressionWrapper, Q, BooleanField, Exists
+from django.db.models import Q
+import datetime
 
 
 @login_required
 def rewards_homepage(request):
     user_claims = RewardClaim.objects.filter(user=request.user).values_list('reward_type__reward_id', flat=True)
     rewards = Reward.objects.all().annotate(claimed=Q(reward_id__in=list(user_claims)))
-    return render(request, 'rewards/rewards_home.html', { 'rewards': rewards, 'user': request.user })
+    date = datetime.date.today()
+    return render(request, 'rewards/rewards_home.html', { 'rewards': rewards, 'date': date })
 
 @login_required
 def claim_reward(request, brand_name, reward_id):
@@ -21,7 +23,8 @@ def claim_reward(request, brand_name, reward_id):
     user_claims = RewardClaim.objects.filter(user=request.user).values_list('reward_type__reward_id', flat=True)
     reward = Reward.objects.get(reward_id=reward_id)
     if reward.reward_id in user_claims:
-        return redirect('rewards')
+        reward_claim = RewardClaim.objects.get(user=request.user, reward_type__reward_id=reward_id)
+        return render(request, 'rewards/rewards_claim.html', { 'reward': reward, 'reward_claim': reward_claim })
     
     reward_claim = RewardClaim.objects.create(reward_type=reward, user=request.user)
     return render(request, 'rewards/rewards_claim.html', { 'reward': reward, 'reward_claim': reward_claim })
@@ -30,6 +33,7 @@ def claim_reward(request, brand_name, reward_id):
 def my_rewards(request):
     user_claims = RewardClaim.objects.filter(user=request.user).values_list('reward_type__reward_id', flat=True)
     claimed_rewards = Reward.objects.filter(Q(reward_id__in=list(user_claims)))
-    return render(request, 'rewards/my_rewards.html', { 'claimed_rewards': claimed_rewards })
+    date = datetime.date.today()
+    return render(request, 'rewards/my_rewards.html', { 'claimed_rewards': claimed_rewards, 'date': date })
     
 
