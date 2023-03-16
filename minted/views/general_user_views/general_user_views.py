74868facfb0e4a1120a06e886c0122c53a001231
Yu Han Chen <yu.h.chen@kcl.ac.uk>
2023-03-16 21:58:32 +0000

    Fix bug where superuser cannot login
    - Stop superuser streak updates

--- minted/views/general_user_views/general_user_views.py
+++ minted/views/general_user_views/general_user_views.py
@@ -9,10 +9,6 @@ from minted.views.general_user_views.login_view_functions import *
 from minted.views.general_user_views.point_system_views import *
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.hashers import check_password
-from datetime import datetime, timedelta
-from django.utils import timezone
-import pytz
-
 from django.conf import settings
 from minted.notifications import unsubscribe_user_from_push, is_user_subscribed
 
