88f3c8da441fca2fe3ad843204bb9b500ff171ac
Yasamin Divsalar <yasamindivsalar@gmail.com>
2023-03-17 13:07:29 +0000

    "Update reward budget points view
    -get rid of global variable and implement a get all budgets function"

--- minted/views/budget_views.py
+++ minted/views/budget_views.py
@@ -3,19 +3,26 @@ from django.contrib.auth.decorators import login_required
 from ..models import *
 from django.contrib import messages
 from .budget_views_functions import *
-from minted.views.general_user_views.point_system_view_function import all_budgets
     
+
+def generate_budget_list(request, categories):
+    all_budgets = []
+
+    for category in categories:
+        all_budgets.append(current_category_limit(category))
+    all_budgets.append(current_user_limit(request.user))
+
+    return all_budgets
+
+
+
+
 @login_required
 def budget_list(request):
     current_user = request.user
     categories = current_user.get_categories()
 
-    if len(all_budgets) != 0:
-        all_budgets.clear()
-
-    for category in categories:
-        all_budgets.append(current_category_limit(category))
-    all_budgets.append(current_user_limit(request.user))
+    all_budgets = generate_budget_list(request, categories)
 
     return render(request, 'budget_list.html', {'budget': all_budgets})
 
--- minted/views/general_user_views/point_system_views.py
+++ minted/views/general_user_views/point_system_views.py
@@ -1,6 +1,5 @@
 from minted.models import *
 from minted.views.budget_views import *  
-from minted.views.general_user_views.point_system_view_function import all_budgets
 import datetime
 from datetime import datetime, timedelta
 from django.utils import timezone
@@ -23,7 +22,7 @@ def standardise_timeframe(category):
     return yearly_budget
 
 
-def calculate_category_weightings(request, category):
+def calculate_category_weightings(request, category, all_budgets):
     user = request.user
     user_total_budget = all_budgets[-1]
     if (user.budget.timeframe != '/year'):
@@ -33,26 +32,22 @@ def calculate_category_weightings(request, category):
    
     category_object = Category.objects.get(user = user.id, name = category.name)
     if category_object.budget.timeframe != '/year':
-        yearly_budget = standardise_timeframe(request, category_object)
-        weighting_of_category = yearly_budget / user_yearly_budget
+        category_yearly_budget = standardise_timeframe(category_object)
+        weighting_of_category = category_yearly_budget / user_yearly_budget
     else:
         weighting_of_category = float(category.budget) / user_yearly_budget
     return weighting_of_category
 
 
-def calculate_budget_points(request):
-    user = request.user
+def calculate_budget_points(request, all_budgets, category):
     total_budget_reward = 100
-    for category in all_budgets[:-1]:
-        weighting_of_category = calculate_category_weightings(request, category)
-        now = str(timezone.now().date())
-        
-        if (now == category.end_date) and (category.spent <= category.budget):
-            points_reward = math.ceil(weighting_of_category * total_budget_reward)
-            user.points += points_reward
-            user.save()
+    weighting_of_category = calculate_category_weightings(request, category, all_budgets)
+    points_reward = math.ceil(weighting_of_category * total_budget_reward)
+    return points_reward
 
-def is_today_a_end_date():
+def is_today_a_end_date(request):
+    categories = request.user.get_categories()
+    all_budgets = generate_budget_list(request, categories)
     today = datetime.now().date()
     ends_today = []
     for category in all_budgets:
@@ -64,9 +59,17 @@ def is_today_a_end_date():
 
 
 def reward_budget_points(request):
-    budget_list(request)
+    user = request.user
+    categories = user.get_categories()
+    all_budgets = generate_budget_list(request, categories)
+    now = str(timezone.now().date())
     if (all_budgets is not None) and (len(all_budgets) > 1):
-        calculate_budget_points(request)
+        for category in all_budgets[:-1]:
+            if category.spent <= category.budget and now == category.end_date:
+                reward_points = calculate_budget_points(request, all_budgets, category)
+                user.points += reward_points
+                user.save()
+
        
 
 def update_streak(request, user):  
@@ -78,15 +81,17 @@ def update_streak(request, user):
         user.streak_data.streak = 1
         user.streak_data.last_login_time = user.last_login
         reward_points_daily(request)
-        ends_today = is_today_a_end_date()
+        ends_today = is_today_a_end_date(request)
         if ends_today.contains(True):
             reward_budget_points(request)
     elif window_size <= time_since_last_login < 2 * window_size:
         user.streak_data.streak += 1
         user.streak_data.last_login_time = user.last_login
         reward_points_daily(request)
-        ends_today = is_today_a_end_date()
+        ends_today = is_today_a_end_date(request)
         if ends_today.contains(True):
             reward_budget_points(request)
+
+    reward_budget_points(request)
         
     user.streak_data.save()
