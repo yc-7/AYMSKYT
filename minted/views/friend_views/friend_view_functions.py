d28ac52080a0badbf394810e11baa1ea5553e7f4
Yu Han Chen <yu.h.chen@kcl.ac.uk>
2023-03-24 18:56:32 +0000

    Refactor friend views
    - Move some functionality to seperate methods in friend_view_functions

--- minted/views/friend_views/friend_view_functions.py
+++ minted/views/friend_views/friend_view_functions.py
@@ -1,5 +1,6 @@
 
-from minted.models import Subscription
+from minted.models import FriendRequest, Subscription
+from minted.notifications import send_push
 
 
 def user_is_subscribed_to_friend_notifications(user):
@@ -17,3 +18,23 @@ def user_is_subscribed_to_friend_notifications(user):
 def make_friends(user1, user2):
     user1.friends.add(user2)
     user2.friends.add(user1)
+
+def send_friend_request_notification(from_user, to_user):
+    head = "You have received a friend request"
+    body = f"{from_user} has send you a friend request"
+    send_push(head, body, to_user.id)
+
+def can_send_friend_request(from_user, to_user):
+    sent_to_self = (to_user == from_user)
+    has_incoming_request = FriendRequest.objects.filter(from_user = to_user, to_user = from_user).count() != 0
+    request_already_sent = FriendRequest.objects.filter(from_user = from_user, to_user = to_user).count() != 0
+
+    if sent_to_self or request_already_sent or has_incoming_request:
+        return False
+    
+    return True
+
+def send_friend_request_accept_notification(to_user, from_user):
+    head = "Your friend request has been accepted!"
+    body = f"You and {to_user.first_name} are now friends!"
+    send_push(head, body, from_user.id)
--- minted/views/friend_views/friend_views.py
+++ minted/views/friend_views/friend_views.py
@@ -4,8 +4,6 @@ from minted.forms import *
 from minted.models import *
 from ..general_user_views.login_view_functions import *
 from django.contrib import messages
-from django.core.exceptions import ValidationError
-from minted.notifications import send_push
 from minted.views.friend_views.friend_view_functions import *
 
 @login_required
@@ -16,33 +14,25 @@ def friend_request(request):
         if form.is_valid():
             email = form.cleaned_data.get('email')
             from_user = request.user
-            is_active = form.cleaned_data.get('is_active')
             recipient_does_not_exist = User.objects.filter(email=email).count() == 0
 
             if recipient_does_not_exist:
                 messages.add_message(request, messages.ERROR, "User does not exist")
                 return redirect('friend_request') 
             
-            recipient = User.objects.get(email=email)
-            sent_to_self = (recipient == request.user)
-            has_incoming_request = FriendRequest.objects.filter(from_user = recipient, to_user = request.user).count() != 0
-            request_already_sent = FriendRequest.objects.filter(from_user = request.user, to_user = recipient).count() != 0
+            to_user = User.objects.get(email=email)
             
-
-            if sent_to_self or request_already_sent or has_incoming_request:
+            if not can_send_friend_request(from_user, to_user):
                 messages.add_message(request, messages.ERROR, "You cannot send a friend request to this user!")
                 return redirect('friend_request') 
-            
-            to_user = User.objects.get(email = email)
-            new_friend = FriendRequest.objects.create(
+
+            FriendRequest.objects.create(
                 from_user = from_user,
                 to_user = to_user,
                 is_active = True
             )
-            if user_is_subscribed_to_friend_notifications(recipient):
-                head = "You have received a friend request"
-                body = f"{from_user} has send you a friend request"
-                send_push(head, body, to_user.id)
+            if user_is_subscribed_to_friend_notifications(to_user):
+                send_friend_request_notification(from_user, to_user)
         return redirect('profile') 
     
     return render(request, 'friend_request.html', {'form': form})
@@ -50,10 +40,10 @@ def friend_request(request):
 @login_required
 def accept_request(request, friend_request_id):
     if request.method == 'POST':
-        if FriendRequest.objects.filter(id = friend_request_id).count() == 0:
+        user_has_no_friend_requests = FriendRequest.objects.filter(id = friend_request_id).count() == 0
+        if user_has_no_friend_requests:
             messages.add_message(request, messages.ERROR, "Invalid request!")
             return redirect('request_list')
-
 
         friend_request = FriendRequest.objects.get(id = friend_request_id)
 
@@ -62,7 +52,6 @@ def accept_request(request, friend_request_id):
 
         make_friends(from_user, to_user)
 
-        friend_request.is_active = False
         friend_request.delete()
         messages.add_message(request, messages.SUCCESS, "Friend request accepted!")
 
@@ -70,9 +59,7 @@ def accept_request(request, friend_request_id):
             return redirect('request_list')
 
         if user_is_subscribed_to_friend_notifications(to_user):
-            head = "Your friend request has been accepted!"
-            body = f"You and {to_user.first_name} are now friends!"
-            send_push(head, body, from_user.id)
+            send_friend_request_accept_notification(to_user, from_user)
 
         return redirect('request_list')
 
@@ -80,7 +67,6 @@ def accept_request(request, friend_request_id):
 def decline_request(request, friend_request_id):
     if request.method == 'POST':
         friend_request = FriendRequest.objects.get(id = friend_request_id)
-        friend_request.is_active = False
         friend_request.delete()
         messages.add_message(request, messages.SUCCESS, "Friend request declined!")
         return redirect('request_list')
@@ -96,8 +82,8 @@ def friend_list_view(request):
 @login_required
 def request_list_view(request):
     current_user = request.user
-    requests_sent_to_me_list = FriendRequest.objects.filter(to_user = current_user)
-    context = {'user': current_user,'requests': requests_sent_to_me_list}
+    friend_requests_sent_to_current_user = FriendRequest.objects.filter(to_user = current_user)
+    context = {'user': current_user,'requests': friend_requests_sent_to_current_user}
     return render(request, 'request_list.html', context)
                 
 @login_required
