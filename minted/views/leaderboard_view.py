0e61fb3bf232421507902aaf7f35699ecf96bea5
Thang Nguyen <thang.1.nguyen@kcl.ac.uk>
2023-03-24 18:34:11 +0000

    Make leaderboard only display the user and friends
    - Make the leaderboard view only display the user and their friends
    - Make leaderboard display rank based on points and streaks
    - Add relation name in user model
    - Create base_leaderboard.html and have points and streaks leaderboard html pages extend it

--- minted/views/leaderboard_view.py
+++ minted/views/leaderboard_view.py
@@ -6,15 +6,40 @@ class LeaderboardView(LoginRequiredMixin, ListView):
     """View that displays the leaderboard"""
 
     model = User
-    template_name = 'leaderboard.html'
     context_object_name = 'users'
 
     def get_queryset(self):
-        """"""
+        """Return"""
 
-        users = User.objects.all().order_by('-streak_data')
-        return users
+        current_user = self.request.user
+        friends = list(current_user.friends.all()) + [current_user]
+        return friends
+    
+class PointsLeaderboardView(LeaderboardView):
 
+    template_name = 'leaderboards/points_leaderboard.html'
+    
+    def get_queryset(self):
+        friends = super().get_queryset()
+        friends.sort(reverse = True, key = self.getPoints)
+        return friends
+    
+    def getPoints(self, user):
+        """Return a user's points"""
+
+        return user.points
+    
+class StreaksLeaderboardView(LeaderboardView):
+
+    template_name = 'leaderboards/streaks_leaderboard.html'
+    
+    def get_queryset(self):
+        friends = super().get_queryset()
+        friends.sort(reverse = True, key = self.getStreaks)
+        return friends
+    
+    def getStreaks(self, user):
+        """Return a user's streaks"""
+
+        return user.streak_data.streak
         
-
-
