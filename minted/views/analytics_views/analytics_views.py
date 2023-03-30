672429f723a0c669773e53f5fd021a40203f14dd
Yasamin Divsalar <yasamindivsalar@gmail.com>
2023-03-30 14:36:19 +0100

    "Refactor analytics page"

--- minted/views/analytics_views/analytics_views.py
+++ minted/views/analytics_views/analytics_views.py
@@ -2,7 +2,7 @@ from minted.decorators import staff_prohibited
 from django.shortcuts import render
 from minted.forms import TimeFrameForm
 from minted.models import Category
-from minted.views.analytics_views.analytics_view_functions import generate_category_line_chart_dataset, generate_category_pie_chart_dataset
+from minted.views.analytics_views.analytics_view_functions import *
 import datetime
 
 @staff_prohibited
@@ -32,5 +32,19 @@ def view_analytics(request):
     colours = [category.colour for category in categories]
     category_pie_chart_data = generate_category_pie_chart_dataset(categories, start_date, end_date)
     category_line_chart_data = generate_category_line_chart_dataset(categories, start_date, end_date, time_interval)
+    overall_min_and_max_spending_categories = get_overall_max_and_min_spending_categories(category_pie_chart_data)
+    categories_on_budget_percentage = calculate_categories_on_budget_percentage(request)
+    percent_of_budget_remaining = calculate_percentage_of_budget_remaining(request)
+
+   
     
-    return render(request, 'analytics.html', {'form': form, 'category_pie_chart_data': category_pie_chart_data, 'category_line_chart_data': category_line_chart_data, 'colours': colours, 'time_interval': time_interval, 'start_date':start_date, 'end_date':end_date})
+    
+    return render(request, 'analytics.html', {
+        'form': form, 
+        'category_pie_chart_data': category_pie_chart_data, 
+        'category_line_chart_data': category_line_chart_data, 
+        'colours': colours, 'time_interval': time_interval, 
+        'start_date':start_date, 'end_date':end_date, 
+        'extreme_labels':overall_min_and_max_spending_categories,
+        'categories_on_budget_percentage': categories_on_budget_percentage,
+        'percent_of_budget_remaining': percent_of_budget_remaining})
